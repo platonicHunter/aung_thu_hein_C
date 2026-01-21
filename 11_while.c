7c35a501870c9130f2fdc35826c65d957c554067
platonic <minlu0954@gmail.com>
2026-01-21 19:34:49 +0630

    while homework

--- 11_while.c
+++ 11_while.c
@@ -14,13 +14,26 @@ int main()
     // 3 * 3=9
     // 3 * 4=12
     // 3 * 5=15
-    int fixmultiple = 12, count = 1;
+    // int fixmultiple = 12, count = 1;
 
-    while (count < 13)
+    // while (count < 13)
+    // {
+    //     printf("%d * %d = %d\n", fixmultiple, count, count * fixmultiple);
+    //     count++;
+    // }
+
+    int num = 1;
+    while (num < 9)
     {
-        printf("%d * %d = %d\n", fixmultiple, count, count * fixmultiple);
-        count++;
+        printf("%d Hello World\n", num);
+        num++;
     }
 
-    return 0;
+    while (num <= 8)
+    {
+        printf("%d Hello World\n", num);
+        num++;
+    }
+
+        return 0;
 }
---
+++ Homework/1_while.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    /* code */
+    // input from user (integer)
+    // sum of 1 to input number
+    // output the sum
+    return 0;
+}
