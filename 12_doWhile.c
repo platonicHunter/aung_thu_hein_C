6335c93aa55b931cda5d4ca16389012858d1fc56
platonic <minlu0954@gmail.com>
2026-01-26 18:48:28 +0630

    do while loooping

---
+++ 12_doWhile.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    /* code */
+    // do{
+    // statement;
+    // }
+    // while (condition);
+
+    int n = 10;
+    // while (n < 5)
+    // {
+    //     printf("%d Hello World\n", n);
+    //     n++;
+    // }
+
+    do
+    {
+        printf("%d Hello World\n", n);
+        n++;
+    } while (n < 5);
+
+    return 0;
+}
