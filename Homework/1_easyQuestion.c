e9cbc6c3621bb6015621ad3122b408d6943a7081
platonic <minlu0954@gmail.com>
2026-01-26 19:59:58 +0630

    looping exercise

--- Homework/1_easyQuestion.c
+++ Homework/1_easyQuestion.c
@@ -2,14 +2,47 @@
 
 int main()
 {
-    Print numbers from 1 to 10 using a for loop.
+    // // Print numbers from 1 to 10 using a for loop.
+    // // for looping
+    // printf("For looping with 1 to 10\n");
+    // for (int i = 1; i <= 10; i++)
+    // {
+    //     printf("%d\n", i);
+    // }
+    // // while looping
+    // int i = 1;
+    // printf("While looping with 1 to 10\n");
+    // while (i < 11)
+    // {
+    //     printf("%d\n", i);
+    //     i++;
+    // }
 
-Print numbers from 10 to 1 using a for loop.
+    // // Print numbers from 10 to 1 using a for loop.
+    // // while
+    // int c = 10;
+    // printf("While looping with 10 to 1\n");
+    // while (c > 0)
+    // {
+    //     printf("%d\n", c);
+    //     c--;
+    // }
+    // printf("For looping with 10 to 1\n");
+    // for (int i = 10; i >= 1; i--)
+    // {
+    //     printf("%d\n", i);
+    // }
 
-Print even numbers from 1 to 20.
+    // Print even numbers from 1 to 20.
+    printf("For looping with even numbers from 1 to 20\n");
+    for (int i = 2; i < 21; i += 2)
+    {
+        printf("%d\n", i);
+    }
 
-Print odd numbers from 1 to 20.
+    // Print odd numbers from 1 to 20.
 
-Print your name 5 times using a loop.
+    // Print your name 5 times using a loop.
+
     return 0;
 }
