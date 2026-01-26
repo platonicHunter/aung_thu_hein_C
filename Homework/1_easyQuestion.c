193228ce665e01714f4bfb73274d778c6be34b53
platonic <minlu0954@gmail.com>
2026-01-26 19:16:00 +0630

    quwstion

---
+++ Homework/1_easyQuestion.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    Print numbers from 1 to 10 using a for loop.
+
+Print numbers from 10 to 1 using a for loop.
+
+Print even numbers from 1 to 20.
+
+Print odd numbers from 1 to 20.
+
+Print your name 5 times using a loop.
+    return 0;
+}
