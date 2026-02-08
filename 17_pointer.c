6e97673c8675a373d0846d856a8a3d01a60244c2
platonic <minlu0954@gmail.com>
2026-02-08 18:18:10 +0630

    pointer

---
+++ 17_pointer.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    // pointer(&)=> a "variable-like" taht holds a memory address
+
+    int age = 2;
+    printf("%d\n", age);
+    printf("address : %p\n", &age); // memory address
+
+    // pointer declaration(*)
+    int *pAge = &age;
+    printf("value of pAge : %p\n", pAge);
+    printf("value of stored address pAge : %d\n", *pAge);
+    return 0;
+}
