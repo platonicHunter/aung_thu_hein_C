d28a4eee84d691740c806fee1f61d1901c9f8691
platonic <minlu0954@gmail.com>
2026-02-14 21:49:55 +0630

    string

---
+++ 19_string.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    char name[] = "Aung Thu";
+    // character pointer
+    // char *name = "Aung Thu";
+    printf("%s\n", name);
+
+    // fputs(write)(stdout => write in terminal)
+    fputs("Hello\n", stdout);
+
+    // fgets(read)
+    char word[20];
+    char *p;
+    p = &word[0];
+    printf("Enter word: ");
+    fgets(p, 20 * sizeof(char), stdin);
+    printf("%s\n", word);
+    return 0;
+}
